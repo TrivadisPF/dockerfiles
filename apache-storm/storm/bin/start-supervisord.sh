547ebdec0c5ce3c81ff3b601c3b9461d02d32744
Guido Schmutz <guido.schmutz@hotmail.com>
2016-05-04 16:36:37 +0200

    change to WURSTMEISTER base for storm

--- apache-storm/storm/bin/start-supervisord.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/bin/configure-storm.sh
-supervisord
--- apache-storm/storm/config-supervisord.sh
+++ apache-storm/storm/config-supervisord.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 echo [program:storm-$1] | tee -a /etc/supervisor/conf.d/storm-$1.conf
 echo command=storm $1 | tee -a /etc/supervisor/conf.d/storm-$1.conf
 echo directory=/home/storm | tee -a /etc/supervisor/conf.d/storm-$1.conf
