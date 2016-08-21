4fa904e4228c493017d172ee572f0f705af3fde2
Guido Schmutz <guido.schmutz@hotmail.com>
2016-08-21 13:36:57 +0200

    remove build folder

--- datastax/dse/build/docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Set the DSE_VERSION so it can be used then run docker build
-. "`dirname $0`/DSE_VERSION"
-
-docker build --build-arg DSE_VERSION=$DSE_VERSION -t luketillman/datastax-enterprise:$DSE_VERSION .
