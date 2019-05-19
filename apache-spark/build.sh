7c4ae586bda82d5b9eb5fcc24d862181bdc1bc3b
Guido Schmutz <guido.schmutz@trivadis.com>
2019-05-19 17:19:57 +0200

    intitial for apache-spark

---
+++ apache-spark/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+TAG=2.4.0-hadoop3.1
+
+build() {
+    NAME=$1
+    IMAGE=trivadis/spark-$NAME:$TAG
+    cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
+    echo '--------------------------' building $IMAGE in $(pwd)
+    docker build -t $IMAGE .
+    cd -
+}
+
+build base
+build master
+build worker
+build submit
+build java-template template/java
+build scala-template template/scala
+build python-template template/python
